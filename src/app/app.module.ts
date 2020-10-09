9ffe6b2d3ad8b9ed8963758347d1416f7d77ed3c
SilindaM <silindanemdu@gmail.com>
2020-10-09 17:17:03 +0200

    initial commit

---
+++ src/app/app.module.ts
@@ -0,0 +1,20 @@
+import { BrowserModule } from '@angular/platform-browser';
+import { NgModule } from '@angular/core';
+
+import { AppRoutingModule } from './app-routing.module';
+import { AppComponent } from './app.component';
+import { PropertyListComponent } from './property/property-list/property-list.component';
+
+@NgModule({
+  declarations: [
+    AppComponent,
+    PropertyListComponent
+  ],
+  imports: [
+    BrowserModule,
+    AppRoutingModule
+  ],
+  providers: [],
+  bootstrap: [AppComponent]
+})
+export class AppModule { }
